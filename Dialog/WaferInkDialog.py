315da2c2a457a4e109c38b1c6c7cfbf588ac4ce2
Ethan <Ethan.qiujian@gmail.com>
2021-05-24 19:19:07 -0400

    updated the inkoff dialog with a new icon

--- Dialog/WaferInkDialog.py
+++ Dialog/WaferInkDialog.py
@@ -6,12 +6,54 @@ from UI import Ui_Ink_Dialog
 
 class ShowInkDialog(QDialog, Ui_Ink_Dialog):
 
+    ink_signal = pyqtSignal(dict)
+
     def __init__(self):
         super(QDialog, self).__init__()
 
         Ui_Ink_Dialog.__init__(self)
 
         self.setupUi(self)
+
+        self.ink_selection = {
+            "auto_ink": False,
+            "click_die_ink": self.toolButton_click.isChecked(),
+            "drag_ink": self.toolButton_drag.isChecked(),
+            "polygon_ink": self.toolButton_polygon.isChecked(),
+        }
+
+        self.toolButton_click.clicked.connect(self.emitInkSignal)
+        self.toolButton_drag.clicked.connect(self.emitInkSignal)
+        self.toolButton_polygon.clicked.connect(self.emitInkSignal)
+        self.toolButton_reset.clicked.connect(self.emitInkSignal)
+        self.toolButton_auto_ink.clicked.connect(self.emitInkSignal)
+
+
+    def emitInkSignal(self):
+        """
+        change the tool states on toolButton
+        :return:
+        """
+        # click and drag mode can only either one be use
+
+        if self.toolButton_click.isChecked():
+            self.toolButton_drag.setChecked(False)
+
+        if self.toolButton_drag.isChecked():
+            self.toolButton_click.setChecked(False)
+            self.toolButton_polygon.setChecked(False)
+
+        if self.toolButton_polygon.isChecked():
+            self.toolButton_drag.setChecked(False)
+
+        self.ink_selection = {
+            "auto_ink": False,
+            "click_die_ink": self.toolButton_click.isChecked(),
+            "drag_ink": self.toolButton_drag.isChecked(),
+            "polygon_ink": self.toolButton_polygon.isChecked(),
+        }
+        self.ink_signal.emit(self.ink_selection)
+
 
 
 
